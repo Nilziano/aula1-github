cbd4918bd77124ead9fc96e8a371ba9100d5620b
Nilziano <nilziano@gmail.com>
2020-03-17 15:55:12 -0300

    add mensagem bom dia

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,9 +1,10 @@
 package Application;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("Bom Dia");
 
 	}
 
